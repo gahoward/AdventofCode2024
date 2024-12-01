4a112142352869868dd57e4c1aa4b3154386dffb
Giles Howard <giles@giles.tech>
2024-12-01 18:23:16 +0000

    Updated to use map/reduce/filter/etc to solve this

--- day1/main.rs
+++ day1/main.rs
@@ -5,15 +5,31 @@ use std::str::Lines;
 fn main() {
     let input_file_location: String = String::from("input.txt");
     let lines: Vec<String> = file_to_vector(input_file_location);
-    let two_lists = extract_lists(lines);
-    let mut list1 = two_lists.0;
-    let mut list2 = two_lists.1;
-    list1 = sort_list(list1);
-    list2 = sort_list(list2);
-    let difference_vec = difference_calculation(list1, list2);
-    let final_value: i32 = difference_vec.iter().sum();
-    //Ta-da!
-    println!("Final value is: {}", final_value);
+    
+    /* 
+    First attempt before I realised I could do this as a Vector of Tuples and use map/reduce/etc.
+    
+    //let two_lists = extract_lists(lines);
+    //let mut list1 = two_lists.0;
+    //let mut list2 = two_lists.1;
+    //list1 = sort_list(list1);
+    //list2 = sort_list(list2);
+    //let difference_vec = difference_calculation(list1, list2);
+    //let final_value: i32 = difference_vec.iter().sum();
+    //println!("Final value is: {}", final_value);
+
+    */
+
+    let two_lists = extract_lists_tuples(lines);
+    println!("two_list values is: {:?}", two_lists);
+
+    let part1_value: i32 = two_lists.iter().map(|x| (x.0 - x.1).abs()).sum();
+     println!("Part 1 value is: {}", part1_value);
+
+     let part2_value: i32 = similarity_calculator(two_lists);
+     println!("Part 2 value is: {}", part2_value);
+
+
 }
 
 //Read file line-by-line, push each line into a Vector of type String, which we will subsequently manipulate into two independent lists of i32s.
@@ -27,6 +43,10 @@ fn file_to_vector(file_path: String) -> Vec<String> {
 
     return line_vec;
 }
+
+/*
+
+Original solution, before I realised we could simply do map/reduce/filter/fold on Tuples.
 
 //Produce two Vectors in a tuple, each representing one column of digits (as i32s), eliminating the whitespace in the input that separates the two values.
 fn extract_lists(input: Vec<String>) -> (Vec<i32>, Vec<i32>)
@@ -70,4 +90,51 @@ fn difference_calculation(list1: Vec<i32>, list2: Vec<i32>) -> Vec<i32> {
     }
     
     return difference_vec;
+}
+
+    */
+
+//This seems more idiomatic: we return a Vector of (i32, i32) tuples;
+fn extract_lists_tuples(input: Vec<String>) -> Vec<(i32, i32)>
+{
+    let mut unsorted_left: Vec<i32> = Vec::new();
+    let mut unsorted_right: Vec<i32> = Vec::new();
+
+
+    //Line by line iteration.
+    for line in input {
+    //Horribly unreadable mess but: split each string into an iterator, providing 2 elements sans whitespace use map to parse each element into a number, returning (hopefully!) two numeric values.
+    let mut parts = line.split_whitespace().map(|s| s.parse::<i32>());
+    match (parts.next(), parts.next()) {
+        (Some(Ok(a)), Some(Ok(b))) => {
+            unsorted_left.push(a);
+            unsorted_right.push(b);
+        }
+        //Code will panic here if we get a malformed list ... but the input is well-formed so I'm not doing any additional checks here.
+        _ => {} 
+    }
+    }
+
+    //Sort both lists as it's required for the first part of the task, and doesn't harm the second part.
+    unsorted_left.sort();
+    unsorted_right.sort();
+
+    //This ensures that the lowest index of the Vector has the lowest values of each of the two lists, the second lowest has the second lowest of the two lists, etc.
+    let mut combined_vector: Vec<(i32, i32)> = Vec::new();
+    for (pos, element) in unsorted_left.iter().enumerate() {
+        combined_vector.push((*element, *unsorted_right.get(pos).unwrap()));
+    }
+
+    //Return the Vector of Tuples;
+    return combined_vector
+}
+
+//Mostly I am showing off here.
+fn similarity_calculator(input: Vec<(i32, i32)>) -> i32 {
+    let mut similarity = 0;
+    for (pos, element) in input.iter().enumerate() {
+        similarity += element.0 * input.iter().map(| x | x.1 - element.0).filter(| y | *y == 0).fold(0, | acc, element | acc + 1 );
+        
+    }
+    return similarity
 }
